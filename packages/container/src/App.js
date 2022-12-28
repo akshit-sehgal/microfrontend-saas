01aa45d008ef63d02df3a042cdb176485c58eb90
akshit-sehgal <akshitsehgal95@gmail.com>
2022-12-28 21:29:36 +0530

    build: dummy commit to trigger github workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>SaaS Product</h1>
+      <h1>SaaS Product Demo</h1>
       <hr />
       <MarketingApp />
     </div>
