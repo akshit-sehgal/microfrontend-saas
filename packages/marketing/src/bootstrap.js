d9b10ce679492a7e45a682fb8f7879dcf49533cb
akshit-sehgal <akshitsehgal95@gmail.com>
2022-12-31 20:05:14 +0530

    feat(container,marketing): adding support for multi-tier navigation & container-subapp communication

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 import App from './App';
 
-const mount = (element) => {
+const mount = (element, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (typeof onNavigate === 'function') {
+    history.listen(onNavigate);
+  }
+
   ReactDOM.render(
-    <App />,
+    <App history={history} />,
     element
   );
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  };
 };
 
 if (process.env.NODE_ENV === 'development') {
   const devRootElement = document.querySelector('#__marketing-root');
   if (devRootElement) {
-    mount(devRootElement);
+    mount(devRootElement, { defaultHistory: createBrowserHistory() });
   }
 }
 
