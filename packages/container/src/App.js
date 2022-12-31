d46d68e634947e255c47d1b2795186975eccb54f
akshit-sehgal <akshitsehgal95@gmail.com>
2022-12-31 14:35:08 +0530

    chore: dummy commit to trigger build

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
-console.log('updates');
-
 export default () => {
   return (
     <div>
--- packages/container/src/index.js
+++ packages/container/src/index.js
@@ -1,1 +1,2 @@
+console.log('updates');
 import('./bootstrap');
