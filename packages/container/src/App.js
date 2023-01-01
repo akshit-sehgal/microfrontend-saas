32275fa02f1ce5144372fd7302377185f785ec35
akshit-sehgal <akshitsehgal95@gmail.com>
2023-01-01 22:35:57 +0530

    feat(dashboard): integrating dashboard app with the container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,22 +1,33 @@
-import React, { lazy, Suspense, useState } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
+import { Switch, Route, Router, Redirect } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
+import { createBrowserHistory } from 'history';
 
 import Header from './components/Header';
 import Progress from './components/Progress';
 
 const AuthAppLazy = lazy(() => import('./components/AuthApp'));
 const MarketingAppLazy = lazy(() => import('./components/MarketingApp'));
+const DashboardAppLazy = lazy(() => import('./components/DashboardApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard');
+    }
+  }, [isSignedIn])
+
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
@@ -25,11 +36,16 @@ export default () => {
               <Route path='/auth' >
                 <AuthAppLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path='/dashboard'>
+                {
+                  !isSignedIn ? <Redirect to="/" /> : <DashboardAppLazy />
+                }
+              </Route>
               <Route path='/' component={MarketingAppLazy} />
             </Switch>
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
